0df6a8f4bb0ed8aed44417763e9e5ca233b90364
mandarin <hoojunee@gmail.com>
2022-12-05 15:05:52 +0900

    templates_22.12.05

--- template/index.php
+++ template/index.php
@@ -20,28 +20,48 @@
       <i class="fa-solid fa-chevron-right"></i>
     </button>
   </div><!-- slider -->
-</section>
-<script>
-  $('.home-visual .col_width').append('<ul class="img_container"></ul>')
-  subSliderArr.forEach(function(v) {
-    $('.home-visual .col_width ul').append(`
+  <script>
+    $('.home-visual .col_width').append('<ul class="img_container"></ul>')
+    subSliderArr.forEach(function(v) {
+      $('.home-visual .col_width ul').append(`
       <li>
-        <figure class="inner">
+      <figure class="inner">
           <img src="./img/sub/${v.path}" alt>
           <figcaption>
-            <em><span>${v.title}</span></em>
-            <p><span>${v.desc}</span></p>
+          <em><span>${v.title}</span></em>
+          <p><span>${v.desc}</span></p>
           </figcaption>
-        </figure>
-      </li>
-    `) //append
-  }) //forEach}append)
-</script>
-<main>
-  <section class="home_section home_section1"></section>
+          </figure>
+          </li>
+          `) //append
+    }) //forEach}append)
+  </script>
+  <main>
+</section>
+<section class="home_section home_section1">
   <div class="main_desc">
     <h2>quick menu</h2>
     <p>메뉴를 클릭하시면 해당 메뉴로 이동합니다.</p>
   </div>
+</section>
+<script>
+  $('.home_section1').append(`<ul></ul>`)
+  window.quickMenuArr.forEach(function(v) {
+    $('.home_section1 ul').append(`
+      <li>
+        <a href="${v.url}">
+          <figure>
+            <img src="${v.path}" alt>
+            <svg viewBox="0 0 300 300">
+            <path d="M224 21.8 76 21.8 2 150 76 278.2 224 278.2 298 150z" />
+            </svg>
+          </figure>
+          <h3>${v.title}</h3>
+          <p>${v.desc}</p>
+        </a>
+      </li>
+    `)
+  }) //forEach
+</script>
 </main>
 <?php include "footer.php" ?>
