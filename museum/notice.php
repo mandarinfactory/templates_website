0c7111c45a80cdf1956845031a958c7764e600e8
mandarin <hoojunee@gmail.com>
2022-12-06 14:09:24 +0900

    templates_22.12.06

--- museum/notice.php
+++ museum/notice.php
@@ -4,8 +4,8 @@
     <figure>
         <img src="./img/museum/museum_night.jpg" alt="">
         <figcaption>
-            <li>역사와 문화가 살아 숨 쉬고,</li>
-            <li>과거와 현재, 미래가 공존하는 감동의 공간인</li>
+            <li><b>역사와 문화</b>가 살아 숨 쉬고,</li>
+            <li><b>과거와 현재, 미래가 공존<b>하는 감동의 공간인</li>
             <li>국립중앙박물관에 오신 것을</li>
             <li>환영합니다.</li>
         </figcaption>
