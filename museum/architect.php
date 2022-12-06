acee8bd9c9c431c4594570620bdd29c3e48a90ec
mandarin <hoojunee@gmail.com>
2022-12-06 17:10:10 +0900

    templates_22.12.06

--- museum/architect.php
+++ museum/architect.php
@@ -11,7 +11,13 @@
             <li>연면적</li>
             <li>138,156.25㎡</li>
             <li>구조</li>
-            <li></li>
+            <li>철골·철근콘크리트조</li>
+            <li>층수</li>
+            <li>지하1층, 지상6층</li>
+            <li>최고높이</li>
+            <li>43.08m</li>
+            <li>설계자</li>
+            <li>정림건축</li>
         </figcaption>
     </figure>
 </section>
