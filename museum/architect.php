044d36ae0f8d52829937c25ee7b3775ff28d7fec
mandarin <hoojunee@gmail.com>
2022-12-06 17:07:00 +0900

    templates_22.12.06

--- museum/architect.php
+++ museum/architect.php
@@ -4,12 +4,13 @@
     <figure>
         <img src="./img/museum/landscape.jpg" alt="">
         <figcaption>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>대지면적</li>
+            <li>295,550.69㎡</li>
+            <li>건축면적</li>
+            <li>48,644.47㎡</li>
+            <li>연면적</li>
+            <li>138,156.25㎡</li>
+            <li>구조</li>
             <li></li>
         </figcaption>
     </figure>
