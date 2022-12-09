f8a76fd362d4e00717f48ad204fefced821c10b0
mandarin <hoojunee@gmail.com>
2022-12-09 10:57:47 +0900

    templates_22.12.09

--- wordpress/wp-content/themes/theme_template/header.php
+++ wordpress/wp-content/themes/theme_template/header.php
@@ -4,13 +4,20 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css" .css>
   <title><?php bloginfo('name'); ?> <?php wp_title('|'); ?></title>
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
   <header>
-    <?php if (function_exists('bcn_display')) {
-      bcn_display();
-    } ?>
+    <h1><a href="<?php bloginfo('url'); ?>">사이트제목</a></h1>
+    <nav>
+      <?php wp_nav_menu(array('theme_location' => 'menu')); ?>
+    </nav>
+    <nav>
+      <?php if (function_exists('bcn_display')) {
+        bcn_display();
+      } ?>
+    </nav>
   </header>
