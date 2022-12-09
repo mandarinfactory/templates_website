9c2d3d4dafc40790e4fbd9f2c746b7a6dbc20f58
mandarin <hoojunee@gmail.com>
2022-12-09 15:39:48 +0900

    templates_22.12.09

--- museum/js/index.js
+++ museum/js/index.js
@@ -1,4 +1,5 @@
 $(function () {
     responsive_carousel_horz(".home_visual", false, 5000, 7000)
+    responsive_carousel_horz(".home_section2 .slider", false, 5000, 7000)
     responsive_carousel_vert(".home_section3 .slider", false, 3000, 5000)
 })//document.ready
