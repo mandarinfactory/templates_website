f677e0eb31013e58b46130c75c218b32199e2a31
mandarin <hoojunee@gmail.com>
2022-12-12 11:37:32 +0900

    templates_22.12.12

--- wordpress/wp-content/themes/theme_template/page.php
+++ wordpress/wp-content/themes/theme_template/page.php
@@ -1,5 +1,5 @@
 <?php include 'sub_header.php' ?>
 <!--블로그 글 출력 시작-->
-page출력 본문입니다!!!!!!!
+<b>page출력 본문입니다!!!!!!!</b>
 <!--블로그 글 출력 끝-->
 <?php include 'sub_footer.php' ?>
--- wordpress/wp-content/themes/theme_template/single.php
+++ wordpress/wp-content/themes/theme_template/single.php
@@ -1,5 +1,5 @@
 <?php include 'sub_header.php' ?>
 <!--블로그 글 출력 시작-->
-single출력 본문입니다!!!!!!!
+<b>글 제목과 글 본문이 나옵니다!!!!!!!</b>
 <!--블로그 글 출력 끝-->
 <?php include 'sub_footer.php' ?>
