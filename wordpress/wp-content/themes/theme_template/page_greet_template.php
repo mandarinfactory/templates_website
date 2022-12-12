b3a18ccfe56dfa28c63b6eff6ff9c1e22d2101b9
mandarin <hoojunee@gmail.com>
2022-12-12 12:51:51 +0900

    templates_22.12.12

---
+++ wordpress/wp-content/themes/theme_template/page_greet_template.php
@@ -0,0 +1,13 @@
+<?/*
+    Template Name: greet_design
+    Template Post Type: page, greet_template
+*/ ?>
+<?php include 'sub_header.php' ?>
+<!--블로그 글 출력 시작-->
+<b>인사말 page template!!!!!</b>
+<h2>인사말</h2>
+<section>
+	ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎ
+</section>
+<!--블로그 글 출력 끝-->
+<?php include 'sub_footer.php' ?>
