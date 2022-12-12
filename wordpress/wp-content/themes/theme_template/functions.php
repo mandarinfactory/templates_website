da7cfc708001b6f841fdf8ed974ea70fcc426653
mandarin <hoojunee@gmail.com>
2022-12-12 09:59:49 +0900

    templates_22.12.12

--- wordpress/wp-content/themes/theme_template/functions.php
+++ wordpress/wp-content/themes/theme_template/functions.php
@@ -1,3 +1,15 @@
 <?php
 
-?>
+/* thumbnail */
+add_theme_support('post-thumbnails');
+
+/* navigation */
+register_nav_menus(array(
+    'sitemap' => 'sitemap',
+));
+
+/* widget */
+register_sidebar(array(
+    'name' => __('searchWidget'),
+    'id' => 'searchWidget',  // 위젯 영역의 고유한 ID
+));
