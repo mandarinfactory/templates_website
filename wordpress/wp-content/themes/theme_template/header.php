ffd9a1be3f4cf05fd97dc00c377f35f755b48c29
mandarin <hoojunee@gmail.com>
2022-12-12 11:02:57 +0900

    templates_22.12.12

--- wordpress/wp-content/themes/theme_template/header.php
+++ wordpress/wp-content/themes/theme_template/header.php
@@ -4,11 +4,17 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?> //테마 url/css/style.css">
   <title><?php bloginfo('name'); ?> <?php wp_title('|'); ?></title>
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
   <header>
-    header
+    <h1><a href="<?php bloginfo('url'); ?>">wordpress연습!</a></h1>
   </header>
+  <hr>
+  <nav class="gnb">
+    <b>GNB_메뉴</b>
+    <?php wp_nav_menu(array('theme_location' => 'sitemap')); ?>
+  </nav>
