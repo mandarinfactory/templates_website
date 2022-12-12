36ee4986475e3a81604304a5563243282b70b76f
mandarin <hoojunee@gmail.com>
2022-12-12 10:52:58 +0900

    templates_22.12.12

--- museum/js/store.js
+++ museum/js/store.js
@@ -23,7 +23,8 @@ window.subVisualArr = [
     {
         pic : '1.jpg',
         title : 'Ipsum Lorem',
-        desc : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        desc : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        
     },
     {
         pic : '2.jpg',
