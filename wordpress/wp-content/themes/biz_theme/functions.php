acc1594bce10d1396e8ce60642c29ddb6db5071f
mandarin <hoojunee@gmail.com>
2022-12-13 11:00:23 +0900

    templates_22.12.13

--- wordpress/wp-content/themes/biz_theme/functions.php
+++ wordpress/wp-content/themes/biz_theme/functions.php
@@ -1,3 +1,9 @@
 <?php
-
-?>
+add_theme_support('post-thumbnails'); // 대표이미지추가
+register_nav_menus(array(
+    'menu' => 'menu',
+)); // 메뉴기능추가
+register_sidebar(array(
+    'name' => __('위젯이름'),
+    'id' => 'widjet1',  // 위젯 영역의 고유한 ID
+)); // 위젯추가
