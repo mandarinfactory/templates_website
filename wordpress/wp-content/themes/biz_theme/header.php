872f2ab1778f9c0d0cb872f13348238f8a7fa0b9
mandarin <hoojunee@gmail.com>
2022-12-13 13:03:34 +0900

    templates_22.12.13

--- wordpress/wp-content/themes/biz_theme/header.php
+++ wordpress/wp-content/themes/biz_theme/header.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="./css/reset.css">
-<link rel="stylesheet" href="./css/style.css">
-<script src="https://kit.fontawesome.com/7d1f9ca95d.js" crossorigin="anonymous"></script>
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script src="./js/common.js"></script>
-<html <?php language_attributes(); ?> class="no-js">
 
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
   <title><?php bloginfo('name'); ?> <?php wp_title('|'); ?></title>
+  <html <?php language_attributes(); ?> class="no-js">
   <?php wp_head(); ?>
+  <link rel="stylesheet" href="<?php bloginfo('url'); ?>css/reset.css">
+  <link rel="stylesheet" href="<?php bloginfo('url'); ?>css/style.css">
+  <script src="https://kit.fontawesome.com/7d1f9ca95d.js" crossorigin="anonymous"></script>
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+  <script src="<?php bloginfo('url'); ?>/js/common.js"></script>
 </head>
 
 <body <?php body_class(); ?>>
