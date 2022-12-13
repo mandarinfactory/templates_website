11eaa182f6906fde127942e60c322c37198c63d8
mandarin <hoojunee@gmail.com>
2022-12-13 13:18:25 +0900

    templates_22.12.13

--- wordpress/wp-content/themes/biz_theme/header.php
+++ wordpress/wp-content/themes/biz_theme/header.php
@@ -9,12 +9,13 @@
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Song+Myung&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="<?php bloginfo('url'); ?>reset.css">
-  <link rel="stylesheet" href="<?php bloginfo('url'); ?>style.css">
-  <link rel="stylesheet" href="<?php bloginfo('url'); ?>mixin.css">
+  <link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('url'); ?>/img/icon&logo/favicon.ico">
+  <link rel="stylesheet" href="<?php bloginfo('url'); ?>/css/reset.css">
+  <link rel="stylesheet" href="<?php bloginfo('url'); ?>/css/style.css">
+  <link rel="stylesheet" href="<?php bloginfo('url'); ?>/css/mixin.css">
   <script src="https://kit.fontawesome.com/7d1f9ca95d.js" crossorigin="anonymous"></script>
   <script src="http://code.jquery.com/jquery-latest.js"></script>
-  <script src="<?php bloginfo('url'); ?>common.js"></script>
+  <script src="<?php bloginfo('url'); ?>/js/common.js"></script>
 </head>
 
 <body <?php body_class(); ?>>
