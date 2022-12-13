00ac54d376f58a2b4e873ef886d00835c115baa7
mandarin <hoojunee@gmail.com>
2022-12-13 13:01:42 +0900

    templates_22.12.13

--- wordpress/wp-content/themes/biz_theme/header.php
+++ wordpress/wp-content/themes/biz_theme/header.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="./css/reset.css">
+<link rel="stylesheet" href="./css/style.css">
+<script src="https://kit.fontawesome.com/7d1f9ca95d.js" crossorigin="anonymous"></script>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="./js/common.js"></script>
 <html <?php language_attributes(); ?> class="no-js">
 
 <head>
