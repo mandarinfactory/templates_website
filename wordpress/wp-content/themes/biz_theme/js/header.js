a6671093456b045b5ce1aa18cd465fe6fe623bc9
mandarin <hoojunee@gmail.com>
2022-12-14 15:53:13 +0900

    templates_22.12.14

--- wordpress/wp-content/themes/biz_theme/js/header.js
+++ wordpress/wp-content/themes/biz_theme/js/header.js
@@ -2,11 +2,14 @@ $(function () {
     /* menu_btn ------------------------------------------------- */
     $('.menu_btn').click(function () {
         $(this).addClass('active')
+
     })//click
     /* gnb_1dep ------------------------------------------------- */
     $('.gnb > div > ul > li > a').click(function (e) {
         e.preventDefault();
         $('.gnb > div > ul > li > ul').stop().slideUp()
-        $(this).siblings('ul').stop().slideDown()
+        $(this).siblings('ul').stop().slideToggle()
+        $('.gnb > div > ul > li > a').not($(this)).removeClass('active')
+        $(this).toggleClass('active')
     })//click
 })//document.ready
