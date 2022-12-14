b5b2249f58a2cbc13c2ef0a7dfde97088108fde9
mandarin <hoojunee@gmail.com>
2022-12-14 10:46:03 +0900

    templates_22.12.14

--- wordpress/wp-content/themes/biz_theme/header.php
+++ wordpress/wp-content/themes/biz_theme/header.php
@@ -26,10 +26,10 @@
     <a href="<?php bloginfo('url'); ?>">
       <div class="logo">
         <figure class="cube">
-          <img class="front" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-front.png" alt>
-          <img class="back" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-back.png" alt>
-          <img class="left" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-left.png" alt>
-          <img class="right" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-right.png" alt>
+          <img class="front" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-front-back.png" alt>
+          <img class="back" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-front-back.png" alt>
+          <img class="left" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-left-right.png" alt>
+          <img class="right" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-left-right.png" alt>
           <img class="top" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-top-bottom.png" alt>
           <img class="bottom" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/icon&logo/logo-top-bottom.png" alt>
         </figure>
