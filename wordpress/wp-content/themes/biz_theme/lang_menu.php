fa53a3bdfc30268d74b62974cad0007596f2cfa8
mandarin <hoojunee@gmail.com>
2022-12-16 09:32:19 +0900

    wordpress_22.12.16

--- wordpress/wp-content/themes/biz_theme/lang_menu.php
+++ wordpress/wp-content/themes/biz_theme/lang_menu.php
@@ -1,10 +1,17 @@
 <script>
     $(function() {
         $('.lang button').click(function() {
+<<<<<<< HEAD
+            var n = $(this).val()
+            $('.lang').removeClass('active1 active2 active3').addClass('active' + n)
+        })
+    })
+=======
             var n = $(this).val();
             $('.lang').removeClass('active1 active2 active3').addClass('active' + n);
         })//click_event
     })//document.ready
+>>>>>>> e98aa7940078d9752fb0208cb14bb934f473e5ec
 </script>
 <nav class="lang active1 title=" 해당 UI는 디자인설계까지만 구현되어있습니다.">
     <button value="1">KR</button>
