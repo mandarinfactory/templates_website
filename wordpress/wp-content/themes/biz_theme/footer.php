cf38e78744b68e7fe869365b65e06039a01cf750
mandarin <hoojunee@gmail.com>
2022-12-16 10:41:56 +0900

    wordpress_22.12.16

--- wordpress/wp-content/themes/biz_theme/footer.php
+++ wordpress/wp-content/themes/biz_theme/footer.php
@@ -2,25 +2,19 @@
 
 </div>
 <footer>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
-  footer <br>
+  <div class="footer_top">
+
+  </div>
+  <div class="footer_bottom">
+    <nav class="terms">
+      <a href="#">이용약관</a>
+      <a href="#">개인정보처리방침</a>
+    </nav>
+    <ul class="search_widget">
+      <li></li>
+    </ul>
+    <p class="copyright">Copyright © web-site wordpress.</p>
+  </div>
 </footer>
 <!--site-footer -->
 <?php wp_footer(); ?>
--- wordpress/wp-content/themes/biz_theme/index.php
+++ wordpress/wp-content/themes/biz_theme/index.php
@@ -2,60 +2,7 @@
 
 <!-- website center!!!! start -->
 <div class="body_wrap">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-    Officia nesciunt atque quae deleniti eaque nihil soluta nisi quibusdam provident! Nostrum amet doloremque dolore praesentium magni sit unde. Suscipit, odit amet! <br>
+
 </div>
 <!-- website center!!!! end -->
 
