f8543eaa0592399d086f1cf66c64f0a57578c24a
mandarin <hoojunee@gmail.com>
2022-12-16 09:41:28 +0900

    wordpress_22.12.16

--- wordpress/wp-content/themes/biz_theme/lang_menu.php
+++ wordpress/wp-content/themes/biz_theme/lang_menu.php
@@ -6,7 +6,7 @@
         })
     })
 </script>
-<nav class="lang active1 title=" 해당 UI는 디자인설계까지만 구현되어있습니다.">
+<nav class="lang active1">
     <button value="1">KR</button>
     <button value="2">EN</button>
     <button value="3">JP</button>
