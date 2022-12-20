6f96047bd5a9659185c222e733b8ebed6d5cde24
mandarin <hoojunee@gmail.com>
2022-12-20 12:03:34 +0900

    wordpress_22.12.20

--- wordpress/wp-content/themes/biz_theme/js/sub_visual.js
+++ wordpress/wp-content/themes/biz_theme/js/sub_visual.js
@@ -3,8 +3,15 @@ $(function () {
     var menuNumber
     window.menuCateArr.forEach(function (v) {
         if (thisUrl.match(v.pageLabel)) {
-            menuNumber = v.pageID
+            menuNumber = v.pageID;
         }//if
     })//forEach
-    $('visual' + menuNumber).css({ 'display': 'flex' })
+    $('.sub_visual_img' + menuNumber).css({ 'display': 'flex' });
+    var mainTitleType = new Type_effect(`.sub_visual .sub_visual_img${menuNumber} figcaption h5`, 150)
+    mainTitleType.play();
+    var subTitleType = new Type_effect(`.sub_visual .sub_visual_img${menuNumber} figcaption p`, 50)
+    subTitleType.play();
+    setTimeout(function () {
+        $('.sub_visual_img' + menuNumber).addClass('active');
+    }, 1000)//setTimeout
 })//document.ready
