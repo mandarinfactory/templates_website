2debaab02bb02109be9d9902518f84f0311e3ef0
mandarin <hoojunee@gmail.com>
2022-12-20 10:54:02 +0900

    wordpress_22.12.20

--- wordpress/wp-content/themes/biz_theme/js/store.js
+++ wordpress/wp-content/themes/biz_theme/js/store.js
@@ -2,26 +2,31 @@ window.menuCateArr = [
     {
         pageID: 1,
         pageLabel: '/company/',
+        pageTitle: 'company',
         pageDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
     },
     {
         pageID: 2,
         pageLabel: '/business/',
+        pageTitle: 'business',
         pageDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
     },
     {
         pageID: 3,
         pageLabel: '/product/',
+        pageTitle: 'product',
         pageDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
     },
     {
         pageID: 4,
         pageLabel: '/online/',
+        pageTitle: 'online',
         pageDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
     },
     {
         pageID: 5,
         pageLabel: '/customer/',
+        pageTitle: 'customer',
         pageDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,'
     },
 ]//menuCateArr
--- wordpress/wp-content/themes/biz_theme/sub_visual.php
+++ wordpress/wp-content/themes/biz_theme/sub_visual.php
@@ -1,14 +1,19 @@
 <section class="sub_visual">
-    sub_visual
 </section>
-<figure>
-    <img class="sub_visual_img" src="./img/sub/sub_img1.jpg" alt>
-    <figcaption></figcaption>
-</figure>
 <script>
-    window.menuCateArr.forEach(function(v) {
+    menuCateArr.forEach(function(v) {
         $('.sub_visual').append(`
-            
-        `)
+            <figure class="sub_visual_img${v.pageID}">
+                <img class="bg_img" src="<?php bloginfo('url'); ?>/wp-content/themes/biz_theme/img/img/sub/sub_img${v.pageID}.jpg" alt />
+                <figcaption>
+                    <h5>
+                        <span>${v.pageTitle}</span>
+                    </h5>
+                    <p>
+                        <span>${v.pageDesc}</span>
+                    </p>
+                </figcaption>
+            </figure>
+        `) //append
     }) //forEach
 </script>
