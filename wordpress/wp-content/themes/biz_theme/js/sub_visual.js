4b9090c31a9c64b8894623de2b2e78e21fbb5b57
mandarin <hoojunee@gmail.com>
2022-12-20 11:20:03 +0900

    wordpress_22.12.20

--- wordpress/wp-content/themes/biz_theme/js/sub_visual.js
+++ wordpress/wp-content/themes/biz_theme/js/sub_visual.js
@@ -1,3 +1,10 @@
 $(function () {
-
+    var thisUrl = location.href
+    var menuNumber
+    window.menuCateArr.forEach(function (v) {
+        if (thisUrl.match(v.pageLabel)) {
+            menuNumber = v.pageID
+        }//if
+    })//forEach
+    $('visual' + menuNumber).css({ 'display': 'flex' })
 })//document.ready
