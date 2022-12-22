836a4308ab1f29776c42a11dcc005407ff51296e
mandarin <hoojunee@gmail.com>
2022-12-22 10:54:58 +0900

    wordpress_22.12.22

--- wordpress/wp-content/themes/biz_theme/category-prd_list.php
+++ wordpress/wp-content/themes/biz_theme/category-prd_list.php
@@ -18,32 +18,37 @@
             <i class="fa-solid fa-xmark"></i>
         </button>
     </p>
-    <!--카테고리 설명 출력-->
-    <div class="category_desc">
-        <?php echo category_description(); ?>
-    </div>
-    <!--카테고리 리스트 출력-->
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <!--리스트 한개 출력-->
-            <!--글 이미지-->
-            <?php if (has_post_thumbnail()) {
-                the_post_thumbnail('thumbnail');
-            } ?>
-            <!--글 제목-->
-            <?php the_title(); ?>
-            <!--작성자-->
-            <?php echo get_the_author(); ?>
-            <!--작성일-->
-            <?php echo get_the_date(); ?> <?php echo get_the_time(); ?>
-            <!--요약글 출력-->
-            <?php the_excerpt(); ?>
-            <!--해당글 링크-->
-            <a href="<?php the_permalink(); ?>">해당글 보기</a>
-            <!--리스트 한개 끝-->
-        <?php endwhile;
-    else : ?>
-        <!--카테고리 글이 없는 경우-->
-    <?php endif; ?>
+    <ul>
+        <!--카테고리 설명 출력-->
+        <div class="category_desc">
+            <?php echo category_description(); ?>
+        </div>
+        <!--카테고리 리스트 출력-->
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <li>
+                    <!--리스트 한개 출력-->
+                    <figure>
+                        <!--글 이미지-->
+                        <?php if (has_post_thumbnail()) {
+                            the_post_thumbnail('thumbnail');
+                        } ?>
+                        <!--해당글 링크-->
+                        <a href="<?php the_permalink(); ?>">
+                            <figcaption>
+                                <!--글 제목-->
+                                <?php the_title(); ?>
+                                <!--요약글 출력-->
+                                <?php the_excerpt(); ?>
+                            </figcaption>
+                        </a>
+                    </figure>
+                    <!--리스트 한개 끝-->
+                </li>
+            <?php endwhile;
+        else : ?>
+            <!--카테고리 글이 없는 경우-->
+        <?php endif; ?>
+    </ul>
     <!--카테고리 리스트 출력끝-->
     <!--페이징출력시작-->
     <?php
