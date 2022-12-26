358a2c22e949891bc063d6b8171027896aa0f0dd
mandarin <hoojunee@gmail.com>
2022-12-26 10:18:17 +0900

    wordpress_22.12.26

--- wordpress/wp-content/themes/biz_theme/category-prd-gallery.php
+++ wordpress/wp-content/themes/biz_theme/category-prd-gallery.php
@@ -19,19 +19,66 @@
         </button>
     </p>
 
-    <ul class="list_container">
-        <!--카테고리 설명 출력-->
-        <div class="category_desc">
-            <?php echo category_description(); ?>
-        </div>
 
-        <ul class="tag-cloud">
-            <?php
-            if (is_active_sidebar('tags')) {
-                dynamic_sidebar('tags');
-            }
-            ?>
-        </ul>
+    <!--카테고리 설명 출력-->
+    <div class="category_desc">
+        <?php echo category_description(); ?>
+    </div>
+    <!-- 태그 클라우드 출력 -->
+    <ul class="tag-cloud">
+        <?php
+        if (is_active_sidebar('tags')) {
+            dynamic_sidebar('tags');
+        }
+        ?>
+    </ul>
+    <!-- 목록 ----------------------------------------->
+
+    <!--카테고리 리스트 출력-->
+    <ul class="list-container">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <li>
+                    <!--리스트 한개 출력-->
+                    <div class="inner">
+                        <figure>
+                            <!--글 이미지-->
+                            <?php if (has_post_thumbnail()) {
+                                the_post_thumbnail('thumbnail');
+                            } ?>
+                        </figure>
+                        <div class="textbox">
+                            <!--글 제목-->
+                            <?php the_title(); ?>
+                            <!--요약글 출력-->
+                            <?php the_excerpt(); ?>
+                            <!--해당글 링크-->
+                            <a href="<?php the_permalink(); ?>">해당글 보기</a>
+                            <!--리스트 한개 끝-->
+                        </div><!-- textbox -->
+                    </div><!-- inner -->
+                </li>
+            <?php endwhile;
+        else : ?>
+            <!--카테고리 글이 없는 경우-->
+        <?php endif; ?>
+    </ul>
+    <!--카테고리 리스트 출력끝-->
+    <!--페이징출력시작-->
+    <?php
+    global $wp_query;
+    $big = 999999999;
+    echo paginate_links(array(
+        'type'        => 'list',
+        'base'        => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+        'format'      => '?paged=%#%',
+        'current'     => max(1, get_query_var('paged')),
+        'total'       => $wp_query->max_num_pages,
+        'prev_text'   => __('<'),
+        'next_text'   => __('>'),
+    ));
+    ?>
+    <!--페이징출력끝-->
+
 
 </section>
 <?php include "sub_footer.php" ?>
