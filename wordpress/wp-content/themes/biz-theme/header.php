093925c485107caca6d3f0100e22968d8fd303a4
mandarin <hoojunee@gmail.com>
2022-12-30 11:13:02 +0900

    wordpress_22.12.29
    워드프레스....포트폴리오에 안넣을듯 ㅠ (contact-form 생성 및 CSS 변경)

--- wordpress/wp-content/themes/biz-theme/header.php
+++ wordpress/wp-content/themes/biz-theme/header.php
@@ -11,6 +11,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/animation.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/contact-form.css">
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/scrollbar.css">
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/isotope.css">
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/reset.css">
